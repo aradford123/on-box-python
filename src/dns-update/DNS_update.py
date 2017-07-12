15ecdd1efedda10eec59fc19c784cb42a05aa216
Adam Radford (aradford) <aradford@cisco.com>
2017-07-12 18:20:55 +1000

    inital commit

---
+++ src/dns-update/DNS_update.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+import sys
+import time
+from dns import reversename, resolver
+from dns.resolver import NXDOMAIN
+from dns.resolver import NoAnswer
+from dns.resolver import NoNameservers
+from dns.exception import Timeout
+
+ACLNAME = "canary_ip_in"
+
+# this is the command string to add an ACE to the ACL
+UPDATE_ACL_COMMANDS = """
+ip access-list extended canary_ip_in
+no deny ip any any
+remark %s
+permit ip any host %s
+deny ip any any
+"""
+
+UPDATE_SCRIPT_FIRING_COMMANDS = """
+event manager applet DNS_update
+ event timer countdown time %s
+ action 1.0 cli command "enable"
+ action 1.1 cli command "guestshell run python bootflash:DNS_update.py %s
+"""
+
+from cli import cli
+from cli import configure
+
+# yes this is a global... should change this
+ACL = cli("show ip access-list %s" % ACLNAME).split('\n')
+
+def log(message, severity):
+    '''
+    this can also log to spark.  need to check if a token is present
+    :param message:
+    :param severity:
+    :return:
+    '''
+    cli('send log %d "%s"' % (severity, message))
+
+def dns_lookup(name):
+    log("Looking up %s" % name, 5)
+    try:
+        answers = resolver.query(name, 'A')
+        # we might need ttl
+        print "TTL", answers.rrset.ttl
+        return answers.rrset.ttl, [answer.address for answer in answers]
+
+    except NXDOMAIN:
+        pass
+
+    except NoAnswer:
+        pass
+    except NoNameservers:
+        pass
+    except Timeout:
+        pass
+
+#[ConfigResult(success=True, command='', line=1, output='', notes=None),
+# ConfigResult(success=True, command='ip access-list extended canary_ip_in', line=2, output='', notes=None),
+# ConfigResult(success=True, command='no deny ip any any', line=3, output='', notes=None),
+# ConfigResult(success=True, command='permit ip any host 1.1.1.1', line=4, output='', notes=None),
+# ConfigResult(success=True, command='deny ip any any', line=5, output='', notes=None)]
+def add_acl(ip):
+    '''
+    add an entry to the ACL.  look at success or not of the commands
+    :param ip:
+    :return:
+    '''
+    localtime = time.asctime(time.localtime(time.time()))
+    remark = "Added %s @%s" % (ip,localtime)
+    responses = configure(UPDATE_ACL_COMMANDS % (remark,ip))
+    success = reduce (lambda x, y : x and y, [r.success for r in responses])
+    status = "Success" if success else "Fail"
+    log("adding IP: %s to ACL: status: %s" % (ip, status), 5)
+
+def lookup(ip, acl=ACL):
+    '''
+    looks up the ip address in the ACL.  ACL is global in this case
+    :param ip:
+    :param acl:
+    :return:
+    '''
+    for ace in acl:
+        if ip in ace:
+            return True
+    return False
+
+def check_acl(ips):
+    '''
+    checks to see if the IP is in the ACL, otherwise adds it
+    :param ips:
+    :return:
+    '''
+    for ip in ips:
+        if not lookup(ip):
+            add_acl(ip)
+
+def reschedule(seconds, *args):
+    '''
+    set an EEM countdown timer to run the script again
+    :param seconds:
+    :param *args: the initial args that were passed to the script
+    :return:
+    '''
+    responses = configure(UPDATE_SCRIPT_FIRING_COMMANDS % (seconds, " ".join(args)))
+    success = reduce(lambda x, y: x and y, [r.success for r in responses])
+    status = "Success" if success else "Fail"
+    log("reschedule in : %s seconds: status: %s" % (str(seconds), status), 5)
+
+def main(argv):
+    '''
+    takes a list of DNS names, resolves them and gets all of the IP addresses as well as TTL
+    TTL is used to calculate the next run time.  =min(ttl) +1
+    :param argv: domain names to lookup
+    :return:
+    '''
+    all_ips = []
+    ttls = []
+    for arg in argv:
+        ttl, ips = dns_lookup(arg)
+        all_ips.extend(ips)
+        ttls.append(ttl)
+    try:
+        minttl = min(ttls)
+    except ValueError:
+        log("failed to get TTL", 7)
+    print "All IPs", all_ips
+    print "minTTL", min(ttls)
+    print "ACL was", ACL
+    check_acl(all_ips)
+    reschedule(min(ttls), *argv)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print "Usage: %s domain-name ..."
+    else:
+        main(sys.argv[1:])
---
+++ src/pci-tool/pci_check.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import jtextfsm as textfsm
+from cli import cli, configure
+
+import re
+import sys
+import time
+from argparse import ArgumentParser
+
+
+def is_idle_value(string):
+    # can be 'never' or 00:00:00 or 7d23h etc
+    match = re.match(r'([n]+)d.*|(never)', string)
+    if match:
+        if int(match.group(1) >= 7) or string == "never":
+            return True
+    else:
+        return False
+
+
+def is_idle(input_time, output_time):
+    return is_idle_value(input_time) and is_idle_value(output_time)
+
+def log(message, severity):
+    print(message)
+
+    cli('send log %d "%s"' % (severity, message))
+
+
+def apply_commands(commands):
+    response = configure(commands)
+    for r in response:
+        log(r.__str__(), 5)
+
+def process(re_table, apply_change):
+    exec_commands = []
+    re_table.Reset()
+
+    output = cli("show int | inc Last in|line pro")
+    print (output)
+    localtime = time.asctime(time.localtime(time.time()))
+    description = "description PCIShutdown: %s" % localtime
+    fsm_results = re_table.ParseText(output)
+    for interface in fsm_results:
+        # skip non Ethernet
+        if is_idle(interface[2], interface[3]) and ("Ethernet" in interface[0]):
+            if interface[1] != "administratively":
+                if apply_change:
+                    exec_commands.extend(['interface ' + interface[0], description, 'shutdown'])
+                else:
+                    print("(testmode) would have SHUT %s (%s,%s)" % (interface[0], interface[2], interface[3]))
+
+    print('Commands to run:')
+    print(exec_commands)
+    apply_commands(exec_commands)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Select PCI-TOOL args:  reads a list of devices and credentials from STDIN')
+    parser.add_argument('-a', '--apply', action='store_true',
+                   help="Apply commands to device.  no longer run in testing mode.")
+
+    args = parser.parse_args()
+    template = open("/flash/gs_script/pci-tool/show_int.textfsm")
+    re_table = textfsm.TextFSM(template)
+    process(re_table, args.apply)
