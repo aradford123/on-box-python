28e3dedf5d254e6074bf39996d985a40a944ae01
Adam Radford (aradford) <aradford@cisco.com>
2017-07-14 04:44:32 +1000

    perms

--- src/eem-policy/config_diff.py
+++ src/eem-policy/config_diff.py
@@ -1,8 +1,9 @@
 ::cisco::eem::event_register_syslog pattern "CONFIG_I" maxrun 60
 # this is an example of an EEM policy trigger
+# think there is a permissions isssue in ios.  need to create directory in IOs, but then cannot update it with git
 '''
-need to have the following config in IOS
-event manager directory user policy flash:gs_script/eem-policy
+!need to have the following config in IOS
+event manager directory user policy flash:eem-policy
 event manager policy config_diff.py
 '''
 import cli
