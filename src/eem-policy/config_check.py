99ee560084ea074b78bd3ba9a94d0ed8563bdde3
Adam Radford (aradford) <aradford@cisco.com>
2017-07-14 17:48:21 +1000

    configcheck main

--- src/eem-policy/config_check.py
+++ src/eem-policy/config_check.py
@@ -1,5 +1,7 @@
 ::cisco::eem::event_register_syslog pattern "CONFIG_I" maxrun 60
 # this is an example of an EEM policy trigger
+# based of Joe Clarke version
+# https://github.com/CiscoDevNet/python_code_samples_network/blob/master/eem_configdiff_to_spark/sl_config_diff_to_spark.py
 # think there is a permissions isssue in ios.  need to create directory in IOs, but then cannot update it with git
 # need to copy the script to flash:
 '''
@@ -19,8 +21,15 @@ event manager policy config_check.py
 import eem
 import os
 import sys
+import cli
+import re
 sys.path.append("/flash/gs_script/src")
 from utils.spark_utils import getRoomId, postMessage
+
+# cli version
+BACKUP = "flash:run.last"
+# python version
+PY_BACKUP = "/flash/run.last"
 
 def logSpark(message):
     sparktoken = os.environ.get("SPARKTOKEN")
@@ -28,9 +37,31 @@ def logSpark(message):
         roomId = getRoomId("Sanity", sparktoken)
         postMessage(message, roomId, sparktoken)
 
+def create_backup():
+    result = cli('copy run {bak}'.format(bak=BACKUP))
+    if not "copied " in result:
+        raise IOError('Cannot create backup {}'.format(BACKUP))
+
+def get_diff():
+    # first time  there will be no backup to compare to
+    if os.path.exists(PY_BACKUP):
+        diffs = cli('show archive config diff {bak} system:running-config'.format(bak=BACKUP))
+        if 'No changes were found' in diffs:
+            return
+        diff_lines = re.split(r'\r?\n', diffs)
+        msg = 'Configuration differences between the running config and last backup:\n'
+        msg += '```{}```'.format('\n'.join(diff_lines[:-1]))
+        logSpark(msg)
+    create_backup()
+
+
 def main():
-    eem.action_syslog("config changed")
+    #eem.action_syslog("config changed")
     logSpark("config changed")
+    try:
+        get_diff()
+    except IOError as e:
+        eem.action_syslog(e)
 
 if __name__ == "__main__":
     main()
