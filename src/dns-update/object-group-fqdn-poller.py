122fb91a841396e2b90e23c4c5600af30fc92a7f
Vicente De Luca <vdeluca@zendesk.com>
2017-07-15 12:57:17 -0700

    can't run as daemon, using cisco EEM

--- src/dns-update/object-group-fqdn-poller.py
+++ src/dns-update/object-group-fqdn-poller.py
@@ -11,9 +11,9 @@ from cli import configure
 
 
 '''
-This code is executed as a daemon and takes no parameters.
-It periodically fetch all object-groups starting with FQDN-*
-and sync it up with the corresponding IPs, respecting records TTL
+This code is executed as a one-shot script and takes no parameters.
+It fetches all object-groups starting with FQDN-* and sync them up
+with the corresponding IPs, re-scheduling next shot the min(TTL)
 
 IOS-XE does not allow creation of empty object-groups, so addition
 of new FQDN based objects can be done as the following:
@@ -24,7 +24,12 @@ of new FQDN based objects can be done as the following:
 After the first iteration 127.0.0.1 will be replaced by the resolved IP(s)
 '''
 
-
+UPDATE_SCRIPT_FIRING_COMMANDS = """
+event manager applet FQDN-POLLER
+ event timer countdown time %s
+ action 1.0 cli command "enable"
+ action 1.1 cli command "guestshell run fqdn-poller
+"""
 
 DEBUG = False
 
@@ -64,6 +69,19 @@ def object_group_configure(fqdn,ip,template,action):
     status = "Success" if success else "Fail"
     log("RESOLVER(%s): %s IP: %s - status: %s" % (fqdn, action, ip, status), 5)
 
+def reschedule(seconds):
+    '''
+    set an EEM countdown timer to run the script again
+    :param seconds:
+    :param *args: the initial args that were passed to the script
+    :return:
+    '''
+    responses = configure(UPDATE_SCRIPT_FIRING_COMMANDS % (seconds))
+    success = reduce(lambda x, y: x and y, [r.success for r in responses])
+    status = "Success" if success else "Fail"
+    if DEBUG:
+        log("reschedule in : %s seconds: status: %s" % (str(seconds), status), 5)
+
 
 def main(argv):
     MIN_TTL = 30
@@ -71,35 +89,32 @@ def main(argv):
     OBJECT_GROUP_ADD = """object-group network FQDN-%s\n host %s\n"""
     OBJECT_GROUP_DEL = """object-group network FQDN-%s\n no host %s\n"""
 
-    while True:
+    # Fetch current FQDN object groups
+    object_groups = cli("show object-group | i FQDN-").split('\n')
+    fqdns = [x.split("FQDN-")[1] for x in object_groups if x]
 
-        # Fetch current FQDN object groups
-        object_groups = cli("show object-group | i FQDN-").split('\n')
-        fqdns = [x.split("FQDN-")[1] for x in object_groups if x]
+    for fqdn in fqdns:
+        # DNS Lookup
+        ttl, resolved_ips = dns_lookup(fqdn)
 
-        for fqdn in fqdns:
-            # DNS Lookup
-            ttl, resolved_ips = dns_lookup(fqdn)
+        # Polling interval should consider the minimum original TTL
+        if ttl < MIN_TTL:
+            MIN_TTL = ttl + 1
 
-            # Polling interval should consider the minimum original TTL
-            if ttl < MIN_TTL:
-                MIN_TTL = ttl + 1
+        # Compare resolved IPs vs. object group IPs
+        object_group_ips  = object_group_lookup(fqdn)
+        include           = list(set(resolved_ips) - set(object_group_ips))
+        exclude           = list(set(object_group_ips) - set(resolved_ips))
 
-            # Compare resolved IPs vs. object group IPs
-            object_group_ips  = object_group_lookup(fqdn)
-            include           = list(set(resolved_ips) - set(object_group_ips))
-            exclude           = list(set(object_group_ips) - set(resolved_ips))
+        # Synchronize config based on DNS Lookup IPs
+        for ip in include:
+            object_group_configure(fqdn,ip,OBJECT_GROUP_ADD,"adding")
 
-            # Synchronize config based on DNS Lookup IPs
-            for ip in include:
-                object_group_configure(fqdn,ip,OBJECT_GROUP_ADD,"adding")
+        for ip in exclude:
+            object_group_configure(fqdn,ip,OBJECT_GROUP_DEL,"removing")
 
-            for ip in exclude:
-                object_group_configure(fqdn,ip,OBJECT_GROUP_DEL,"removing")
-
-        # Sleep for minimum record TTL -- NEED FIX: answers.rrset.ttl = time for resolver cache to expire, not original TTL
-        print("Sleeping %s" % (MIN_TTL))
-        time.sleep(MIN_TTL)
+    # Sleep for minimum record TTL -- NEED FIX: answers.rrset.ttl = time for resolver cache to expire, not original TTL
+    reschedule(MIN_TTL)
 
 
 if __name__ == "__main__":
